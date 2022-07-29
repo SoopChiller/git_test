fc077818cc71a7c2872dc3899fa5a03ce240ce37
SooperChiller <superchillermusic@gmail.com>
2022-07-28 17:50:05 -0700

    Add rando.js

---
+++ rando.js
@@ -0,0 +1,12 @@
+function game(player1, player2) {
+    const theMark = Math.floor(Math.random( * 100));
+    console.log (`If the ${theMark} then..`);
+    let random1 = Math.abs(theMark - player1);
+    let random2 = Math.abs(theMark - player2);
+    if (random1 > random2) {
+        return `Player 2 is closest`
+    }
+    else return `Player 1 is closest`
+}
+
+console.log(game(25,63))
